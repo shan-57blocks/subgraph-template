18fbcce69cc8fa76a0863774ce3ed3dcc7f26051
yangshan <jjt-ys@outlook.com>
2024-07-20 22:31:26 +0800

    init project

--- src/erc20-event-handler.ts
+++ src/erc20-event-handler.ts
@@ -4,34 +4,36 @@ import { Transfer } from '../generated/ERC20/ERC20';
 import { Account, TransferEvent } from '../generated/schema';
 
 export function handleTransfer(event: Transfer): void {
-  const { from, to, value } = event.params;
-
-  const fromAccountId = from.toHexString();
-  let fromAccount = Account.load(from.toHexString());
+  const fromAccountId = event.params.from.toHexString();
+  let fromAccount = Account.load(event.params.from.toHexString());
   if (!fromAccount) {
     fromAccount = new Account(fromAccountId);
     fromAccount.id = fromAccountId;
     fromAccount.totalAmountReceived = BigInt.fromI32(0);
     fromAccount.totalAmountSent = BigInt.fromI32(0);
   }
-  fromAccount.totalAmountReceived = fromAccount.totalAmountReceived.plus(value);
+  fromAccount.totalAmountReceived = fromAccount.totalAmountReceived.plus(
+    event.params.value
+  );
   fromAccount.save();
 
-  const toAccountId = to.toHexString();
-  let toAccount = Account.load(to.toHexString());
+  const toAccountId = event.params.to.toHexString();
+  let toAccount = Account.load(event.params.to.toHexString());
   if (!toAccount) {
     toAccount = new Account(toAccountId);
     toAccount.id = toAccountId;
     toAccount.totalAmountReceived = BigInt.fromI32(0);
     toAccount.totalAmountSent = BigInt.fromI32(0);
   }
-  toAccount.totalAmountSent = toAccount.totalAmountSent.plus(value);
+  toAccount.totalAmountSent = toAccount.totalAmountSent.plus(
+    event.params.value
+  );
 
   const transferEvent = new TransferEvent(event.transaction.hash.toHexString());
   transferEvent.tx = event.transaction.hash;
-  transferEvent.amount = value;
+  transferEvent.amount = event.params.value;
   transferEvent.timestamp = event.block.timestamp;
-  transferEvent.from = from;
-  transferEvent.to = to;
+  transferEvent.from = event.params.from;
+  transferEvent.to = event.params.to;
   transferEvent.save();
 }
